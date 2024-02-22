c5f77ec9949dd13a896537793ea8c5a2e8331e86
jyson <dkatlf900@gmail.com>
2024-02-22 18:05:58 +0900

    실버-1316-그룹단어체크

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import org.example.bronze1.Bronze1357;
-import org.example.silver5.Silver1010;
-import org.example.silver5.Silver1181;
-import org.example.silver5.Silver1193;
-import org.example.silver5.Silver1427;
+import org.example.silver5.*;
 
 import java.io.IOException;
 
@@ -12,7 +9,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Bronze1357 a = new Bronze1357();
+        Silver1316 a = new Silver1316();
         a.test();
     }
 }
