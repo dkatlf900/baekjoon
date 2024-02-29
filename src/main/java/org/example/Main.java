f94443dde8a5ea8048d455c28c73bf081a3554ad
jyson <dkatlf900@gmail.com>
2024-02-29 14:45:12 +0900

    실버-18258-deque2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.silver4.Silver10845;
 import org.example.silver4.Silver14425;
+import org.example.silver4.Silver18258;
 
 import java.io.IOException;
 
@@ -9,7 +10,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Silver10845 a = new Silver10845();
+        Silver18258 a = new Silver18258();
         a.test();
     }
 }
