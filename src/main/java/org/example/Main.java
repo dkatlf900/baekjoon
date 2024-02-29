0e362e3544316d79ce372a0a421e9891d71cd841
jyson <dkatlf900@gmail.com>
2024-02-29 10:28:24 +0900

    실버-10845-deque자료구조

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.silver4.Silver10845;
 import org.example.silver4.Silver14425;
 
 import java.io.IOException;
@@ -8,7 +9,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Silver14425 a = new Silver14425();
+        Silver10845 a = new Silver10845();
         a.test();
     }
 }
