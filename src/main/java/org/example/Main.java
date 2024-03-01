6573b04e49a35ab19fa51ea87cf7af16216f3042
jyson <dkatlf900@gmail.com>
2024-03-01 12:06:06 +0900

    실버-17219-hash

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.silver4.Silver10845;
-import org.example.silver4.Silver14425;
-import org.example.silver4.Silver18258;
-import org.example.silver4.Silver28278;
+import org.example.silver4.*;
 
 import java.io.IOException;
 
@@ -11,7 +8,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Silver28278 a = new Silver28278();
+        Silver17219 a = new Silver17219();
         a.test();
     }
 }
