9b1ae65671a85eb80ccc9a14ac802b74ddc42957
jyson <dkatlf900@gmail.com>
2024-03-06 00:32:17 +0900

    실버-15651-백트래킹탐색v3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.silver3.Silver15649;
 import org.example.silver3.Silver15650;
+import org.example.silver3.Silver15651;
 
 import java.io.IOException;
 
@@ -9,7 +10,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Silver15650 a = new Silver15650();
+        Silver15651 a = new Silver15651();
         a.test();
     }
 }
